5bdb236943d5c25aad3b263ffeef16b2593cc162
LaurenSUni <lauren.szmandiuk@student.uts.edu.au>
2022-08-28 14:51:05 +1000

    this one is actually 70% lol

--- Assets/Scripts/LoadAssets.cs
+++ Assets/Scripts/LoadAssets.cs
@@ -6,10 +6,16 @@ public class LoadAssets : MonoBehaviour
 {
     public GameObject redObj;
 
+    [SerializeField]
+    private GameObject blueObj;
+
     // Start is called before the first frame update
     void Start()
     {
-
+      //create redObj at (2,0,0) and rotation of 0
+      Instantiate(redObj, new Vector3(2, 0, 0), Quaternion.identity);
+      //create blueObj at (-2,0,0) and rotation 0
+      Instantiate(blueObj, new Vector3(-2, 0, 0), Quaternion.identity);
     }
 
     // Update is called once per frame
