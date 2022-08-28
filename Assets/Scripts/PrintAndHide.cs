ad6601fac9ca669d61d652f46e2e0caba73c2acd
LaurenSUni <lauren.szmandiuk@student.uts.edu.au>
2022-08-28 15:46:17 +1000

    100%

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -6,11 +6,13 @@ public class PrintAndHide : MonoBehaviour
 {
     public int i;
     public Renderer rend;
+    public int randomInt;
 
     // Start is called before the first frame update
     void Start()
     {
       i = 3;
+      randomInt = Random.Range(200, 250);
     }
 
     // Update is called once per frame
@@ -18,5 +20,11 @@ public class PrintAndHide : MonoBehaviour
     {
         i++;
         Debug.Log(gameObject.name + ":" + i);
+        if ((gameObject.tag == "Red") && (i == 100)) {
+          gameObject.SetActive(false);
+        }
+        else if ((gameObject.tag == "Blue") && (i == randomInt)) {
+          rend.enabled = false;
+        }
     }
 }
